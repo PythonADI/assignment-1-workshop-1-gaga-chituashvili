92b471ac26c26fb496da29d86af3004f4c4b2988
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-02-03 01:04:39 +0400

    workshop1

--- homework1/main.py
+++ homework1/main.py
@@ -1,6 +1,31 @@
 def main() -> None:
-    # Write your solution here
-    pass
+    #string
+    first_name = "jon"
+    last_name = "doe"
+    print(first_name, last_name)
+     
+     #int
+    age=23
+    print("i am", age ,"years")
+
+    #float
+    Temperature=20.3
+
+    print("temperature is", Temperature ,"dagree")
+
+    #bolean
+    Temperature=20
+
+    if Temperature>30:
+        print("cxela")
+    else: print("ar")
+
+print(19) #int
+print(3.1) #float
+print("12") #string
+print(False) #bolean
+    
+
 
 
 if __name__ == '__main__':
